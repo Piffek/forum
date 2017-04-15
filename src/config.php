c9faf52fdacaff55d0c5c35cc1407155896ec425
Piffek <patrykpiwko123412@gmail.com>
2017-04-15 13:09:20 +0200

    create config file

--- src/config.php
+++ src/config.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-
+	'post_model' => Piwko\Forum\Models\Posts::class,
+	'categories_model' => Piwko\Forum\Models\Categories::class,
+	'profile_models' => Piwko\Forum\Models\Profile::class,
+	'roles_models' => Piwko\Forum\Models\Roles::class,
+	'subject_models' => Piwko\Forum\Models\Subject::class,
+	'roles_has_user_models' => Piwko\Forum\Models\Subject::class,
 ];
